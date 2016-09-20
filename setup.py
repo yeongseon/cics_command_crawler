d477b231bb7ba3bd0149efe1dafebae54e40b152
yeongseon <yeongseon.choe@gmail.com>
2016-09-20 21:34:30 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+#-*- coding: utf-8-*-
+
+from distutils.core import setup
+
+setup(
+        name = 'cccrawer',
+        version = '0.1',
+        py_modules = ['libcccrawler'],
+        packages = ['cccrawler'],
+        
+        )
+        
